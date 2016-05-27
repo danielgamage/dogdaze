80ab071b34102fcebd74f1f2472b99571c830ad9
Daniel Gamage <hello@danielgamage.com>
2016-05-27 14:32:07 -0400

    setting up prismic javascript sdk

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+// var Prismic = require('prismic.io'); // bower-required
+
+Prismic.api("https://dogdaze.prismic.io/api").then(function(api) {
+  return api.query(""); // An empty query will return all the documents
+}).then(function(response) {
+  console.log("Documents: ", response.results);
+}, function(err) {
+  console.log("Something went wrong: ", err);
+});
