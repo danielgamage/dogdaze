0013014c155c49230f091632563b8f9efa7bffe1
Daniel Gamage <hello@danielgamage.com>
2016-06-01 02:11:57 -0400

    renaming vars and adding code documentation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,37 +12,40 @@ var DogBreedItem = React.createClass({
 
 var Filters = React.createClass({
 	handleChange: function(e) {
-		var stateTag;
-		if (e.currentTarget.name == "tag") {
+		var stateGroup;
+		if (e.currentTarget.name == "group") {
 			if (e.currentTarget.checked) {
-				stateTag = e.currentTarget.value;
+				// use input value if
+				stateGroup = e.currentTarget.value;
 			} else {
-				stateTag = '';
+				stateGroup = '';
 			}
 		} else {
-			stateTag = this.props.filterTag;
+			// use app state as fallback
+			stateGroup = this.props.filterGroup;
 		}
 
 		this.props.onUserInput(
-			this.refs.filterTextInput.value,
-			stateTag
+			this.refs.filterSearchInput.value,
+			stateGroup
 		);
 	},
 	render: function() {
-		var tags = [];
+		// Prismic uses tags, but for users we'll call these "groups"
+		var groups = [];
 		for (let tag of this.props.tags) {
-			tags.push(
+			groups.push(
 				<span key={tag} >
 					<input
 						type="checkbox"
-						name="tag"
-						id={"tag" + tag}
-						ref="filterTagInput"
-						checked={this.props.filterTag === tag}
+						name="group"
+						id={"group" + tag}
+						ref="filterGroupInput"
+						checked={this.props.filterGroup === tag}
 						onChange={this.handleChange}
 						value={tag}
 						/>
-					<label htmlFor={"tag" + tag}>
+					<label htmlFor={"group" + tag}>
 						{tag}
 					</label>
 				</span>
@@ -51,13 +54,13 @@ var Filters = React.createClass({
 		return (
 			<form>
 				<h3>Filter by Group</h3>
-				{tags}
+				{groups}
 				<h3>Or Search for your Dog</h3>
 				<input
 					type="text"
 					placeholder="Search..."
-					value={this.props.filterText}
-					ref="filterTextInput"
+					value={this.props.filterSearch}
+					ref="filterSearchInput"
 					onChange={this.handleChange}
 					/>
 			</form>
@@ -68,44 +71,46 @@ var Filters = React.createClass({
 var FilterableDogApp = React.createClass({
 	getInitialState: function() {
 		return {
-			filterText: '',
-			filterTag: ''
+			filterSearch: '',
+			filterGroup: ''
 		};
 	},
-	handleUserInput: function(filterText, filterTag) {
+	handleUserInput: function(filterSearch, filterGroup) {
 		this.setState({
-			filterText: filterText,
-			filterTag: filterTag
+			filterSearch: filterSearch,
+			filterGroup: filterGroup
 		});
 	},
 	render: function() {
-		var breedArray = [];
-		var tagArray   = [];
+		var breedItems = [];
+		var tagList    = [];
 
+		// for each result
 		for (let result of this.props.data.results) {
 			var breed = {};
 			breed.name = result.data['breed.name'].value[0].text;
 			breed.tag  = result.tags[0];
 			// breed.desc = result.data['breed.short_lede'].value[0].text;
 
-			if (breed.name.toLowerCase().indexOf(this.state.filterText.toLowerCase()) !== -1 && breed.tag.indexOf(this.state.filterTag) !== -1) {
-				breedArray.push(<DogBreedItem breed={breed} key={breed.name}/>);
+			// Add breed to breedItems if it passes filtering
+			if (breed.name.toLowerCase().indexOf(this.state.filterSearch.toLowerCase()) !== -1 && breed.tag.indexOf(this.state.filterGroup) !== -1) {
+				breedItems.push(<DogBreedItem breed={breed} key={breed.name}/>);
 			}
 
-			// Add tag to tagArray if it's not already in there
-			if (tagArray.indexOf(breed.tag) == -1) {
-				tagArray.push(breed.tag);
+			// Add tag to tagList if it's not already in there
+			if (tagList.indexOf(breed.tag) == -1) {
+				tagList.push(breed.tag);
 			}
 		}
 		return (
 			<div>
 				<Filters
-					tags={tagArray}
-					filterTag={this.state.filterTag}
-					filterText={this.state.filterText}
+					tags={tagList}
+					filterGroup={this.state.filterGroup}
+					filterSearch={this.state.filterSearch}
 					onUserInput={this.handleUserInput}
 					/>
-				{breedArray}
+				{breedItems}
 			</div>
 		);
 	}
@@ -114,9 +119,10 @@ var FilterableDogApp = React.createClass({
 // Request content
 Prismic.api("https://dogdaze.prismic.io/api").then(function(api) {
 	return api.query(""); // An empty query will return all the documents
-}).then(function(response) {
+}).then(function(prismicDocuments) {
+	// Render app
 	ReactDOM.render(
-		<FilterableDogApp data={response} />,
+		<FilterableDogApp data={prismicDocuments} />,
 		document.querySelector('.list--breeds')
 	);
 }, function(err) {
